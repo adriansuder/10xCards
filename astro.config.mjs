e91bbda0535e00d0817322169236d59168e3767a
Adrian <adrian.suder@outlook.com>
2025-11-12 19:01:13 +0100

    deploy pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import path from 'path';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     }),
   ],
   output: 'server',
+  adapter: cloudflare(),
   vite: {
     resolve: {
       alias: {
